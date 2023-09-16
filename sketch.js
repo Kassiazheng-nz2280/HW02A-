b75c68f146924f945d5284095f0dc0174068d2f5
Kassia Zheng <nz2280@nyu.edu>
2023-09-16 16:30:26 -0400

    three triangle
    three triangle

--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
 function setup() {
+    createCanvas(322,322);
+    background('white');
 }
 
 function draw() {
+    noStroke();
+    fill(14,28,31);
+    quad(10, 11, 318, 18, 310,315, 9,311);
+
+
+    fill(250,231,201);
+    triangle(13,12, 311,18,217,154);
+    triangle(313,19,311,220,172,220);
+    triangle(103,174,310,315,11,312);
+    triangle(11,109,147,107,9,312);
+    
+
+    
+  
+
+    
 }
